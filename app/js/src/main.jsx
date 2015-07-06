38639a1df5c52425684b9e94d086c26f843f6f00
Adnan Asani <adnan@netconsult.se>
2015-07-06 02:25:45 +0200

    Something simple done

---
+++ app/js/src/main.jsx
@@ -0,0 +1,35 @@
+/*** @jsx React.DOM */
+var App = React.createClass({
+  getInitialState: function() {
+    return {
+      result: []
+    };
+  },
+  render: function() {
+    return (
+      <div>
+        <SearchInput onClick={this.handleResult} />
+        <SearchResultList results={this.state.result} />
+      </div>
+    );
+  },
+  /** Expected to be used only by the searchInput-component */
+  handleResult: function(term) {
+    var updateResult = function(response, textStatus, jqXHR) {
+      this.setState({
+        result: (response instanceof Array) ? response : [response]
+      });
+    }.bind(this);
+
+    $.ajax('http://www.omdbapi.com', {
+      method: 'GET',
+      data: {
+        't': term,
+        'type': 'movie',
+        'plot': 'full'
+      },
+      success: updateResult
+    });
+  }
+});
+React.render(<App />, document.getElementById('main-container'));
